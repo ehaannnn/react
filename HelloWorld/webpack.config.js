e3f51b8854ec9f3a3d69c5da7f57c417c866957e
ehaannnn <ehaannnn@gmail.com>
2017-03-15 23:22:05 +0900

    modify some spelling error in the document and add files

---
+++ HelloWorld/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	entry: './helloworld.js',
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders : [
+		{
+			test : /\.js/,
+			loader : 'babel-loader',
+			exclude : /node_modules/,
+			query : {
+				cacheDirectory : true,
+				presets : ['es2015', 'react']
+			}
+		}
+		]
+	}
+
+}
